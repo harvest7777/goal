c36feb1863db41808890d51237b16bcbc5ce8632
harvest7777 <333harvest.moon333@gmail.com>
2025-09-10 23:30:23 -0700

    chore: bug

--- app/protected/progress/_components/all-reflections-modal.tsx
+++ app/protected/progress/_components/all-reflections-modal.tsx
@@ -30,22 +30,23 @@ export function AllReflectionsModalButton({ sessionData }: AllReflectionsModalPr
             <div className="h-full flex flex-col items-center align-middle justify-center">
                 {sessionData? (
                 <ul className="max-h-full w-full overflow-y-auto flex flex-col mt-5">
-                    {sessionData.map((session) => (
+                    {sessionData.filter((session) => session.goal_name !== null).map((session) => (
                     <li key={session.session_id} className="flex flex-col gap-2 border-b border-b-muted mb-5 pb-5">
-
-                        <div>
+                        { session.goal_name && <div>
                             <p className="text-muted-foreground">time investment: </p>
                             <p>{session.goal_name}</p>
-                        </div>
+                        </div>}
+                      {session.output_description &&
                         <div>
                             <p className="text-muted-foreground">output: </p>
                             <p>{session.output_description}</p>
-                        </div>
+                        </div>}
+                      {session.reflection_description &&
                         <div>
                             <p className="text-muted-foreground">reflection: </p>
                             <p>{session.reflection_description}</p>
                         </div>
-
+                    }
                     </li>
                     ))}
                 </ul>
