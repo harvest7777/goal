91c69f11a620ea085776bdd55e8ad24b24033c5b
harvest7777 <333harvest.moon333@gmail.com>
2025-10-15 22:06:52 -0700

    css

--- app/protected/goals/_components/goal-display.tsx
+++ app/protected/goals/_components/goal-display.tsx
@@ -3,21 +3,28 @@ import { MdOutlineEdit } from "react-icons/md";
 import ToggleFocusButton from "./toggle-focus-button";
 
 interface GoalDisplayProps {
-    goal: Goal;
-    className?: string;
+  goal: Goal;
+  className?: string;
 }
 export default function GoalDisplay({ goal, className }: GoalDisplayProps) {
-    return (
-        <div className={`${!goal.is_focused && 'opacity-30'} border p-4 rounded-md ${className}`}>
-            <h2 className="text-lg font-semibold">{goal.name}</h2>
-            <p className="text-gray-600 whitespace-pre-line">{goal.motivator}</p>
-            <div className="mt-3 flex gap-4">
-                <Link className="text-sm text-muted-foreground flex gap-1 items-center align-middle justify-center" href={`goals/${goal.id}/edit`}>
-                    <MdOutlineEdit />
-                    edit
-                </Link>
-                <ToggleFocusButton goal={goal}/>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className={`${
+        !goal.is_focused && "opacity-30"
+      } border p-4 rounded-md ${className}`}
+    >
+      <h2 className="text-lg font-semibold">{goal.name}</h2>
+      <p className="text-primary/80 whitespace-pre-line">{goal.motivator}</p>
+      <div className="mt-3 flex gap-4">
+        <Link
+          className="text-sm text-muted-foreground flex gap-1 items-center align-middle justify-center"
+          href={`goals/${goal.id}/edit`}
+        >
+          <MdOutlineEdit />
+          edit
+        </Link>
+        <ToggleFocusButton goal={goal} />
+      </div>
+    </div>
+  );
 }
