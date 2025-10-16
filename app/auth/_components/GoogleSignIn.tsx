a76099f8a697f1c9c0fd940de516fb78718a8755
harvest7777 <333harvest.moon333@gmail.com>
2025-10-15 20:59:03 -0700

    landing

--- app/auth/_components/GoogleSignIn.tsx
+++ app/auth/_components/GoogleSignIn.tsx
@@ -3,15 +3,17 @@ import supabase from "@/lib/supabase/supabase";
 import { Button } from "@/components/ui/button";
 
 export default function GoogleSignIn() {
-    const handleClick = () => {
-        supabase.auth.signInWithOAuth({
-            provider: 'google',
-            options: {
-              redirectTo: window.location.origin,
-            },
-        })
-    }
+  const handleClick = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: window.location.origin,
+      },
+    });
+  };
   return (
-    <Button onClick={handleClick} className="bg-blue-500">sign in with google</Button>
+    <Button onClick={handleClick} className="bg-blue-500">
+      sign in with google
+    </Button>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,23 @@ import GoogleSignIn from "./auth/_components/GoogleSignIn";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-black">hi</h1>
-      <GoogleSignIn/>
+    <div className="w-full flex flex-col items-center align-middle justify-center">
+      <div className="sm:w-3/5 w-4/5 mt-20">
+        <h1 className="text-center">hello... again</h1>
+        <br></br>
+        <p>if you&apos;re here you probably already know what this is for</p>
+        <br></br>
+        <p>
+          shortly after discontinuing imalockin i realized i am heavily reliant
+          on time management systems, so i spun this up. super basic version
+          without the realtime stuff... ill reveive the project once i have more
+          time.
+        </p>
+        <br></br>
+        <p>thank you all</p>
+        <p>ryan 🤍</p>
+      </div>
+      <GoogleSignIn />
     </div>
   );
 }
