4565c03a2ae0dbbeca3b93d505b539cede31586c
harvest7777 <333harvest.moon333@gmail.com>
2025-10-15 22:18:28 -0700

    css

--- app/protected/progress/_components/week-progress-page.tsx
+++ app/protected/progress/_components/week-progress-page.tsx
@@ -9,67 +9,95 @@ import { format, isThisWeek } from "date-fns";
 import BorderWrapper from "@/components/ui/border-wrapper";
 
 interface WeekProgressPageProps {
-    display: string;
-    date: Date;
-    className?: string;
+  display: string;
+  date: Date;
+  className?: string;
 }
-export default function WeekProgressPage({ display, className, date }: WeekProgressPageProps) {
-    const { goalData, chartConfig, loading, xFormatter, yFormatter } = useWeeklyChartData (date);
+export default function WeekProgressPage({
+  display,
+  className,
+  date,
+}: WeekProgressPageProps) {
+  const { goalData, chartConfig, loading, xFormatter, yFormatter } =
+    useWeeklyChartData(date);
 
-    if (display !== "week") {
-        return null;
-    }
-    if (!date || loading || !goalData) {
-        return (
-            <div className={`h-[calc(50vh)] flex flex-col items-center align-middle justify-center gap-5`}>
-                <Spinner/>
-            </div>
-        )
-    }
-
+  if (display !== "week") {
+    return null;
+  }
+  if (!date || loading || !goalData) {
     return (
-        <div className={`${className} flex flex-col items-center justify-center gap-5`}>
+      <div
+        className={`h-[calc(50vh)] flex flex-col items-center align-middle justify-center gap-5`}
+      >
+        <Spinner />
+      </div>
+    );
+  }
 
-            <BorderWrapper className="w-full flex flex-col items-center align-middle justify-center">
-            {goalData.totalMinsWorkingThisWeek > 0? (
-                <div>
-                <h2 className="w-full">
-                    <span>you spent </span>
-                    <span className="font-bold">{formatMinutesToHoursAndMinutes(goalData.totalMinsWorkingThisWeek)}</span>
-                    <span> working {isThisWeek(date) ? "this week" : "the week of " + format(date, "MMMM do").toLowerCase()}</span>
-                </h2>
+  return (
+    <div
+      className={`${className} flex flex-col items-center justify-center gap-5`}
+    >
+      {goalData.totalMinsWorkingThisWeek > 0 ? (
+        <div>
+          <h2 className="w-full">
+            <span>you spent </span>
+            <span className="font-bold">
+              {formatMinutesToHoursAndMinutes(
+                goalData.totalMinsWorkingThisWeek
+              )}
+            </span>
+            <span>
+              {" "}
+              working{" "}
+              {isThisWeek(date)
+                ? "this week"
+                : "the week of " + format(date, "MMMM do").toLowerCase()}
+            </span>
+          </h2>
 
-                <div className={`mt-5 grid grid-cols-3 gap-5`}>
-                {
-                    (goalData.goalsToRender!
-                        .filter((goal) => goal.weekly_commitment !== null)
-                        .map((goal) => (
-                        <Card key={goal.id} className={`w-64 ${!goal.is_focused && 'opacity-30'}`}>
-                        <CardHeader>
-                            <CardTitle className="flex justify-between">
-                            <span className="w-3/5 line-clamp-1">{goal.name}</span>
-                            <span className="text-muted-foreground font-normal">{formatMinutesToHoursAndMinutes(goalData.goalToTotalMins[goal.id])}</span>
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <RenderChart
-                                chartData={goalData.goalToChartData[goal.id]}
-                                chartConfig={chartConfig}
-                                total={goalData.goalToTotalMins[goal.id]}
-                                formatX={xFormatter}
-                                formatY={yFormatter}
-                                tickX={undefined}
-                                tickY={4}
-                            />
-                        </CardContent>
-                        </Card>
-                )))}
+          <div className={`mt-5 grid sm:grid-cols-2 md:grid-cols-3  gap-5`}>
+            {goalData
+              .goalsToRender!.filter((goal) => goal.weekly_commitment !== null)
+              .map((goal) => (
+                <div key={goal.id}>
+                  <Card
+                    key={goal.id}
+                    className={`bg-background w-64 ${
+                      !goal.is_focused && "opacity-30"
+                    }`}
+                  >
+                    <CardHeader>
+                      <CardTitle className="flex justify-between">
+                        <span className="w-3/5 line-clamp-1">{goal.name}</span>
+                        <span className="text-muted-foreground font-normal">
+                          {formatMinutesToHoursAndMinutes(
+                            goalData.goalToTotalMins[goal.id]
+                          )}
+                        </span>
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <RenderChart
+                        chartData={goalData.goalToChartData[goal.id]}
+                        chartConfig={chartConfig}
+                        total={goalData.goalToTotalMins[goal.id]}
+                        formatX={xFormatter}
+                        formatY={yFormatter}
+                        tickX={undefined}
+                        tickY={4}
+                      />
+                    </CardContent>
+                  </Card>
                 </div>
-                </div>
-            ):(
-                <p className="w-full text-center">no goal data to show</p>
-            )}
-            </BorderWrapper>
+              ))}
+          </div>
         </div>
-    )
+      ) : (
+        <BorderWrapper className="w-full flex flex-col items-center align-middle justify-center">
+          <p className="w-full text-center">no goal data to show</p>
+        </BorderWrapper>
+      )}
+    </div>
+  );
 }
